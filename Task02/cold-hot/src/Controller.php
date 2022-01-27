5c159cb7b5480cfa2a180d5da099f9bb58140274
Andrey Popov <andrvpopov@gmail.com>
2022-01-27 08:05:27 +0300

    Make db-apps branch

--- Task02/cold-hot/src/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace ssiffonn\cold_hot\Controller;
-    use function ssiffonn\cold_hot\View\showGame;
-
-    function startGame(){
-        echo "Game started".PHP_EOL;
-        showGame();
-    }
-?>
--- Task02/cold-hot/src/View.php
+++
@@ -1,7 +0,0 @@
-<?php
-    namespace ssiffonn\cold_hot\View;
-
-    function showGame(){
-        echo "Game shown".PHP_EOL;
-    }
-?>
